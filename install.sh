cb29f9952324f74b58b27714bb70735fe90b92da
Xavier Clark <70333748+xavierclarkvt@users.noreply.github.com>
2022-01-27 09:15:50 -0500

    quick test

--- install.sh
+++ install.sh
@@ -6,3 +6,42 @@ HOMEBREW_PREFIX="$DOTFILES_DIR/bin/is-supported $DOTFILES_DIR/bin/is-arm64 /opt/
 XDG_CONFIG_HOME="$HOME/.config"
 STOW_DIR=$DOTFILES_DIR
 ACCEPT_EULA=Y
+
+start_sudo () {
+    sudo -v
+	while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+}
+
+core_macos () {
+    echo "good shit, should work I think"
+}
+
+core_linux () {
+    apt-get update
+	apt-get upgrade -y
+	apt-get dist-upgrade -f
+
+    $DOTFILES_DIR/bin/is-executable stow || apt-get -y install stow
+}
+
+start_stow () {
+    for FILE in $$(\ls -A runcom); do if [ -f $(HOME)/$$FILE -a ! -h $(HOME)/$$FILE ]; then \
+		mv -v $(HOME)/$$FILE{,.bak}; fi; done
+	mkdir -p $(XDG_CONFIG_HOME)
+	stow -t $(HOME) runcom
+	stow -t $(XDG_CONFIG_HOME) config
+}
+
+if [[$OS == 'macos']]; then
+    start_sudo
+    core_macos
+    get_packages
+    stow_macos
+    start_stow
+else 
+    core_linux
+    start_stow
+fi
+
+
+
