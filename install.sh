33341c745d0e028dc8cf0ab89ba7eadea22a40b7
Xavier Clark <70333748+xavierclarkvt@users.noreply.github.com>
2022-01-27 10:33:35 -0500

    again

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 DOTFILES_DIR="$(dirname $0)"
 OS="$(""$DOTFILES_DIR""/bin/is-supported ""$DOTFILES_DIR""/bin/is-macos macos linux)"
-HOMEBREW_PREFIX="$("$DOTFILES_DIR""/bin/is-supported ""$DOTFILES_DIR""/bin/is-arm64 /opt/homebrew /usr/local)"
+HOMEBREW_PREFIX="$(""$DOTFILES_DIR""/bin/is-supported ""$DOTFILES_DIR""/bin/is-arm64 /opt/homebrew /usr/local)"
 XDG_CONFIG_HOME="$HOME/.config"
 STOW_DIR=$DOTFILES_DIR
 ACCEPT_EULA=Y
