1258b4d07e2be700ce6ae71caa8c9fb2964e36af
Xavier Clark <70333748+xavierclarkvt@users.noreply.github.com>
2023-04-17 23:39:22 -0400

    remove vscode, add codium

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,10 +3,10 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Launchpad.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
-dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Brave.app"
 dockutil --no-restart --add "/Applications/LibreOffice.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
-dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/VSCodium.app"
 dockutil --no-restart --add "/Applications/Discord.app"
 
 killall Dock
